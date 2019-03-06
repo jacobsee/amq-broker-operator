3d25f9f7eec80da158a15ec84d3dfd6ab72144b3
Jacob See <jasee@redhat.com>
2019-03-06 14:30:35 -0800

    Add checking of faked deployment in unit test.

--- pkg/controller/amqbroker/amqbroker_controller_test.go
+++ pkg/controller/amqbroker/amqbroker_controller_test.go
@@ -1,9 +1,12 @@
 package amqbroker
 
 import (
+	"context"
 	"testing"
 
 	jacobseev1alpha1 "github.com/jacobsee/amq-broker-operator/pkg/apis/jacobsee/v1alpha1"
+	routev1 "github.com/openshift/api/route/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -16,20 +19,26 @@ import (
 func TestAmqbrokerController(t *testing.T) {
 
 	var (
-		name      = "amqbroker-operator"
-		namespace = "amqbroker"
+		name      = "amqbroker"
+		namespace = "amqbrokernamespace"
 		username  = "test_user"
 		password  = "test_pass"
 	)
 
 	brokerResource := &jacobseev1alpha1.AMQBroker{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "AMQBroker",
-			Namespace: "AMQBrokerNamespace",
+			Name:      name,
+			Namespace: namespace,
 		},
 		Spec: jacobseev1alpha1.AMQBrokerSpec{
 			Username: username,
 			Password: password,
+		},
+	}
+	routeResource := &routev1.Route{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
 		},
 	}
 
@@ -39,6 +48,7 @@ func TestAmqbrokerController(t *testing.T) {
 
 	scheme := scheme.Scheme
 	scheme.AddKnownTypes(jacobseev1alpha1.SchemeGroupVersion, brokerResource)
+	scheme.AddKnownTypes(routev1.SchemeGroupVersion, routeResource)
 
 	client := fake.NewFakeClient(objs...)
 
@@ -57,6 +67,12 @@ func TestAmqbrokerController(t *testing.T) {
 	}
 	if res.Requeue {
 		t.Error("reconcile requeued unexpectedly")
+	}
+
+	dep := &appsv1.Deployment{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: "amqbroker-deployment", Namespace: namespace}, dep)
+	if err != nil {
+		t.Fatalf("failure getting deployment: (%v)", err)
 	}
 
 }
